e476c3cc411bdf708c5d1fd8d02ef29938756bfc
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-06-14 21:59:21 +0200

    eslint

--- pages/explore/ExploreIndex.js
+++ pages/explore/ExploreIndex.js
@@ -12,7 +12,6 @@ import { EXPLORE_TABS } from 'constants/explore';
 export default class ExploreIndex extends Page {
   render() {
     const { category, subCategory } = this.props.queryParams;
-
     // This is a temporary variable to show some content
     // eslint-disable-next-line
     const description = `Solution description lorem ipusm casius tesebe erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.`;

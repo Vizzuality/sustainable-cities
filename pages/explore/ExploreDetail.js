7f70625222c7171df12a3787b3cb79f086f2df9a
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-06-19 18:31:46 +0100

    Update the templates

--- pages/explore/ExploreDetail.js
+++ pages/explore/ExploreDetail.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Constants
+import { EXPLORE_TABS } from 'constants/explore';
+
 // components
 import Page from 'pages/Page';
 import Layout from 'components/layout/layout';
+import Cover from 'components/common/Cover';
+import Breadcrumbs from 'components/common/Breadcrumbs';
 
 export default class ExploreDetail extends Page {
+
+  static getCategory(slug) {
+    return EXPLORE_TABS.find(category => category.query.category === slug);
+  }
+
   render() {
     const { category, id, slug } = this.props.queryParams;
+
+    const categoryItem = ExploreDetail.getCategory(category);
+    const breadcrumbs = (
+      <Breadcrumbs
+        items={[
+          {
+            name: categoryItem.label,
+            route: 'explore-index',
+            params: { category }
+          }
+        ]}
+      />
+    );
+
     return (
       <Layout
         title="Explore detail"
         queryParams={this.props.queryParams}
       >
+        <Cover title={id} breadcrumbs={breadcrumbs} />
+
         <h1>Explore list</h1>
         <strong>Category: </strong> {category}<br />
         <strong>Slug: </strong> {slug}<br />
@@ -23,7 +49,5 @@ export default class ExploreDetail extends Page {
 }
 
 ExploreDetail.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  queryParams: PropTypes.object.isRequired,
-  slug: PropTypes.string.isRequired
+  queryParams: PropTypes.object.isRequired
 };

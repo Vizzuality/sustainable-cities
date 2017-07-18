96f45e502d1b96da12e319d7aa1a09c82f5619e4
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-07-18 12:01:28 +0200

    Fixes wrong solution tree

--- utils/project.js
+++ utils/project.js
@@ -10,10 +10,10 @@ const listProjects = projects => projects.map(project => ({
 
 // parses projects in order to populate GridSlider component
 const projectsBySolution = (projects) => {
-  const groupedProjects = groupBy(projects, project => project.category.name);
+  const groupedProjects = groupBy(projects, project => project.category.parent.name);
   return Object.values(groupedProjects).map(solution => ({
-    id: solution[0] ? solution[0].category.id : null,
-    title: solution[0] ? solution[0].category.name : null,
+    id: solution[0] ? solution[0].category.parent.id : null,
+    title: solution[0] ? solution[0].category.parent.name : null,
     children: solution.map(project => ({
       id: project.id,
       title: project.name,

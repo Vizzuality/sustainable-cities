5d279fc256cdcc8fbec49f2f952fa394c53a4ee3
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-07-24 19:20:01 +0200

    Adds temporal fixes detail routes

--- routes.js
+++ routes.js
@@ -9,8 +9,8 @@ routes.add('home', '/', 'homepage/HomepageIndex');
 // EXPLORE
 routes.add('explore-index', '/explore/:category?/:subCategory?/:children?', 'explore/ExploreIndex');
 routes.add('explore-detail', '/:type/:id', 'explore/ExploreDetail');
-routes.add('bme-detail', '/business-model-elements/:id', 'explore/BmeDetail');
-routes.add('solution-detail', '/solutions/:id', 'explore/SolutionDetail');
+routes.add('bme-detail', '/business-model-elements/:id', 'explore/ExploreDetail');
+routes.add('solution-detail', '/solutions/:id', 'explore/ExploreDetail');
 
 // ABOUT
 routes.add('about', '/about/:section?', 'about/AboutIndex');

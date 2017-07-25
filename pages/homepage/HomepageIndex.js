e11f2c38773108ac4dcfc30947898d8f09c5839d
Clara Linos <clara.linos@gmail.com>
2017-07-25 12:37:27 +0200

    home layout

--- pages/homepage/HomepageIndex.js
+++ pages/homepage/HomepageIndex.js
@@ -21,7 +21,7 @@ export default class HomePage extends Page {
 
         <section className="l-header">
           <div className="row">
-            <div className="column large-8">
+            <div className="columns large-8 large-offset-2">
               <h1 className="header -title">An initiative to accelerate the implementation of <span className="-highlited">sustainable urban solutions</span> trough new business models</h1>
             </div>
 
@@ -29,22 +29,61 @@ export default class HomePage extends Page {
         </section>
         <section className="l-main-links">
           <div className="row">
-            <ul className="column large-8">
-              <li className="main-link"><a href="">Explore sustainable projects</a></li>
-              <li className="main-link"><a href="">Design a business model</a></li>
-              <li className="main-link"><a href="">Learn more about the initiative</a></li>
+            <ul>
+              <li className="columns large-4"><Link route="explore-index">
+                <a className="m-main-link">
+                  <p className="c-title -dark -fs-extrabig -fw-light">Explore sustainable projects</p>
+                </a>
+              </Link></li>
+              <li className="columns large-4"><Link route="explore-index">
+                <a className="m-main-link">
+                  <p className="c-title -dark -fs-extrabig -fw-light">Design a business model</p>
+                </a>
+              </Link></li>
+              <li className="columns large-4"><Link route="explore-index">
+                <a className="m-main-link">
+                  <p className="c-title -dark -fs-extrabig -fw-light">Learn more about the initiative</p>
+                </a>
+              </Link></li>
             </ul>
           </div>
         </section>
-        <section className="l-home-content">
+        <section className="l-home-intro">
           <div className="row">
-            <div className="column large-8 centered">
+            <div className="columns large-8 large-offset-2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad modi impedit rerum ab omnis repellat necessitatibus voluptate aperiam laborum! Qui, beatae? Tempora sint placeat adipisci fugit ipsa, similique omnis magnam.
             </div>
           </div>
           <div className="row">
-            <div className="column large-4">
-
+            <div className="columns large-10 large-offset-1">
+              <iframe src="https://player.vimeo.com/video/210677339" width="640" height="360"></iframe>
+            </div>
+          </div>
+          <div className="row">
+            <div className="columns large-8 large-offset-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad modi impedit rerum ab omnis repellat necessitatibus voluptate aperiam laborum! Qui, beatae? Tempora sint placeat adipisci fugit ipsa, similique omnis magnam.
+            </div>
+          </div>
+        </section>
+        <section className="l-home-call-to-action -explore">
+          <div className="row">
+            <div className="columns large-6 large-offset-6">
+              <div className="m-call-to-action">
+                <h3 className="c-title -dark -fs-extrabig -fw-light">Explore projects & business models elements</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa maiores adipisci enim dolore blanditiis, odio doloribus, dicta temporibus facere, ipsum tempora ducimus nobis. Libero ad commodi cumque sit mollitia, facilis.</p>
+                <Link route="explore-index"><a className="c-button -secondary">start exploring</a></Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="l-home-call-to-action -builder">
+          <div className="row">
+            <div className="columns large-6">
+              <div className="m-call-to-action">
+                <h3 className="c-title -dark -fs-extrabig -fw-light">Business models builder</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa maiores adipisci enim dolore blanditiis, odio doloribus, dicta temporibus facere, ipsum tempora ducimus nobis. Libero ad commodi cumque sit mollitia, facilis.</p>
+                <Link route="explore-index"><a className="c-button -secondary">start building</a></Link>
+              </div>
             </div>
           </div>
         </section>

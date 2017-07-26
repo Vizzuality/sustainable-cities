11e287a662eb7ce4725799514674295d75849eeb
David Inga <david.inga@vizzuality.com>
2017-07-26 18:43:17 +0200

    added images

--- utils/project.js
+++ utils/project.js
@@ -1,10 +1,29 @@
+import concat from 'lodash/concat';
+import flatten from 'lodash/flatten';
+
+function joiningProjects(solution) {
+  const result = solution.projects || [];
+  if (solution.children && solution.children.length) {
+    for (let index = 0, len = solution.children.length; index < len; index++) {
+      if (solution.children[index].projects && solution.children[index].projects.length) {
+        const projects = solution.children[index].projects;
+        for (let indexProject = 0, pLen = projects.length; indexProject < pLen; indexProject++) {
+          result.push(projects[indexProject]);
+        }
+      }
+    }
+  }
+  return listProjects(result);
+  // return listProjects(flatten(concat(solution.projects, solution.children.map( c => c.projects ))));
+}
 
 // parses projects in order to populate GridList component
 const listProjects = projects => projects.map(project => ({
   id: project.id,
   title: project.name,
   subtitle: project.cities && project.cities[0] ? project.cities[0].name : null,
-  link: { route: 'solution-detail', params: { id: project.id } }
+  link: { route: 'solution-detail', params: { id: project.id } },
+  image: project.photos && project.photos[0] ? `${process.env.API_URL}${project.photos[0].attachment.medium.url}` : null
 }));
 
 // parses projects in order to populate GridSlider component
@@ -13,7 +32,7 @@ const listsProjectsBySolution = solutions =>
     id: solution.id,
     title: solution.name,
     slug: solution.slug,
-    children: listProjects(solution.projects || [])
+    children: joiningProjects(solution)
   })
 );
 

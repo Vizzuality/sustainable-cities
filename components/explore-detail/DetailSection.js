3999a41d1fcdf7ea06c5ac86bdd743542ceddc37
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-07-27 14:35:11 +0100

    Add customisation option to DetailSection component

--- components/explore-detail/DetailSection.js
+++ components/explore-detail/DetailSection.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 export default function DetailSection(props) {
   return (
-    <div className="c-detail-section">
+    <div className={classnames("c-detail-section", { "-content-separator": props.contentSeparator })}>
       <div className="row">
         <div className="column small-4">
           <h2 className="c-title -dark -fs-extrabig -fw-light">{props.title}</h2>
@@ -20,9 +21,14 @@ export default function DetailSection(props) {
 
 DetailSection.propTypes = {
   title: PropTypes.string.isRequired,
+  contentSeparator: PropTypes.bool,
   children: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element)
   ]).isRequired
+};
+
+DetailSection.defaultProps = {
+  contentSeparator: true
 };

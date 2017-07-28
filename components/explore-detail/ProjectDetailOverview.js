727216c50ce1c5996636e41b6e741aaf19b75a6c
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-07-28 16:15:50 +0100

    bme summary in overview section WIP

--- components/explore-detail/ProjectDetailOverview.js
+++ components/explore-detail/ProjectDetailOverview.js
@@ -1,51 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const summaryItemsPlaceholder = [
-  {
-    title: 'Finance',
-    items: [],
-  },
-  {
-    title: 'Funding',
-    items: [
-      'Membership subscription fees',
-      'User charges',
-      'Advertising revenue',
-      'Public grant',
-      'Donation',
-      'City budget',
-    ]
-  },
-  {
-    title: 'Legal framework',
-    items: [
-      'Procurement contract',
-      'Request for proposal',
-      'Operating concession',
-      'Sponsorship agreement',
-      'Public-private partnership',
-      'Bicycle plan ',
-    ]
-  },
-  {
-    title: 'Investment components',
-    items: [
-      'Sidewalk/plazas',
-      'Space between landscaped areas or adjacent infrastructure',
-      'Dedicated Bicycle Lanes',
-      '3rd Generation',
-      'Road Safety',
-      'Branding',
-      'Bicycle Culture',
-      'Special Equipment',
-      'Scoping, planning, and project design (including feasibility studies)',
-      'Bike and Station Assembly ',
-    ],
-  }
-];
+import BmeOverview from 'components/explore-detail/project/BmeOverview';
 
-export default function ProjectOverview({ project }) {
+export default function ProjectDetailOverview({ project }) {
   return (<div className='solution-detail-overview'>
     <div className='solution-detail-overview-summary'>
       <div className='row'>
@@ -54,9 +12,18 @@ export default function ProjectOverview({ project }) {
         </div>
       </div>
       <div className='row'>
-        <div className='column large-12 c-text -fs-huge -fw-thin'>
+        <div className='column large-12'>
+          <div className='c-detail-section'>
+            <div className='content'>
+              <BmeOverview projectId={project.id} bmes={project.bmeTree} itemWidth='25'  />
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>);
 }
+
+ProjectDetailOverview.propTypes = {
+
+};
--- components/explore-detail/project/BmeOverview.js
+++ components/explore-detail/project/BmeOverview.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-
 import { Link } from 'routes';
 
-export default function BmeOverview({ projectId, bmes }) {
+import { CATEGORY_FIRST_LEVEL_COLORS } from 'constants/category';
+
+export default function BmeOverview({ projectId, bmes, itemWidth }) {
   return (
     <div className="c-bme-overview">
-      {bmes.map((bme, n) => (<div className="c-bme-overview-item" key={bme.id}>
-        <div className={classnames("c-bme-overview-strip", `c-color-${n % 4}`)} />
-        <Link route='solution-detail' params={{ id: projectId, subPage: bme.slug }}>
-          <a className="c-text -fs-extrabig -fw-light">{bme.name}</a>
-        </Link>
-        <ul className="c-text -fs-extrasmall -uppercase -fw-light">
-          {bme.children.length === 0 && (<li className='c-text -uppercase'>n/a</li>)}
-          {bme.children.length > 0 && bme.children.map((child) => (<li key={child.id}>
-            {child.name}
-          </li>))}
-        </ul>
-      </div>))}
+      {bmes.map((bme) => {
+
+        let titleColor = CATEGORY_FIRST_LEVEL_COLORS[bme.slug] || CATEGORY_FIRST_LEVEL_COLORS.default;
+
+        return (<div className={`c-bme-overview-item-${itemWidth}`} key={bme.id}>
+          <div className="c-bme-overview-strip" style={{ backgroundColor: titleColor }} />
+          <Link route='solution-detail' params={{ id: projectId, subPage: bme.slug }}>
+            <a className="c-text -fs-extrabig -fw-light">{bme.name}</a>
+          </Link>
+          <ul className="c-text -fs-extrasmall -uppercase -fw-light">
+            {bme.children.length === 0 && (<li className='c-text -uppercase'>n/a</li>)}
+            {bme.children.length > 0 && bme.children.map((child) => (<li key={child.id}>
+              {child.name}
+            </li>))}
+          </ul>
+        </div>)
+      })}
     </div>
   );
 }
 
 BmeOverview.propTypes = {
-  bmes: PropTypes.array
+  projectId: PropTypes.string.isRequired,
+  bmes: PropTypes.array,
+  itemWidth: PropTypes.oneOf(['25', '50'])
 };
 
 BmeOverview.defaultProps = {
-  bmes: []
+  bmes: [],
+  itemWidth: '50'
 };

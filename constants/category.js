b5efff57d2699b5a185c890796b320c1204b3fbc
David Inga <david.inga@vizzuality.com>
2017-08-04 13:36:51 +0200

    categories and slugs for solutions

--- constants/category.js
+++ constants/category.js
@@ -8,20 +8,28 @@ const CATEGORY_TYPE_CONVERSION = {
 
 const CATEGORY_FIRST_LEVEL_COLORS = {
   default: '#000000',
-  'investment-component': '#f7b50c',
-  'delivery-mechanism': '#28bcd4',
-  'financial-product': '#dd7d5c',
-  'funding-source': '#69e191'
+  'investment-components': '#f7b50c', // investment-component
+  'legal-arragements': '#28bcd4', // delivery-mechanism
+  'financing': '#dd7d5c', // financial-product
+  'bme': '#69e191' // funding-source
 };
 
 const CATEGORY_SOLUTIONS_COLORS = {
-  'bike-sharing-systems': '#28bcd4',
-  'new-efficient-constructions': '#ad6bb7'
+  'bike-sharing-scheme': '#28bcd4', // bike-sharing-systems
+  'efficient-new-buildings': '#ad6bb7', // new-efficient-constructions
+  'bus-rapid-transit-brt': '#dd7d5c', // Transit-oriented developments
+  'low-and-zero-emission-buses': '#69e191', // Low emission buses
+  'municipal-building-retrofits': '#f7b50c', // Building efficiency retrofits
+  'transit-oriented-development-tod': '#fabada' // A new one ^.^
 };
 
 const CATEGORY_ICONS = {
-  'bike-sharing-systems': 'icon-bike',
-  'new-efficient-constructions': 'icon-buildings'
+  'bike-sharing-scheme': 'icon-bike', // bike-sharing-systems
+  'efficient-new-buildings': 'icon-buildings', // new-efficient-constructions
+  'bus-rapid-transit-brt': 'icon-city', // Transit-oriented developments
+  'low-and-zero-emission-buses': 'icon-bus', // Low emission buses
+  'municipal-building-retrofits': 'icon-gov', // Building efficiency retrofits
+  'transit-oriented-development-tod': 'icon-city' // A new one ^.^
 };
 
 export {

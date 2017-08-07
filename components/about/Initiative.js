c6e482e302b0a4587ca7afd4df893a524b4d9dc3
Clara Linos <clara.linos@gmail.com>
2017-08-07 16:51:17 +0200

    about the initiative

--- components/about/Initiative.js
+++ components/about/Initiative.js
@@ -2,6 +2,118 @@ import React from 'react';
 
 export default function Initiative() {
   return (<div className="c-about-content">
-    Initiative
+    <div className="c-detail-section -content-padding">
+      <div className="row">
+        <div className="column small-4">
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Background</h2>
+        </div>
+        <div className="column small-8">
+          <p className="c-text -dark -fs-medium -fw-light">In 2015, <a href="http://www.wrirosscities.org/" target="_blank">WRI Ross Center for Sustainable Cities</a> and <a href="www.c40.org" target="_blank">C40 Cities Climate Leadership Group</a>, funded by the <a href="www.citigroup.com/citi/foundation/" target="_blank">Citi Foundation</a>, teamed up for a new partnership to leverage the expertise of our three organizations – WRI’s analytical and research competencies and long-term engagement with cities, the high-level connection with city leaders of C40 and the Citi Foundation’s urban economic progress agenda.</p>
+          <p className="c-text -dark -fs-medium -fw-light">Financing Sustainable Cities combines knowledge sharing between cities and investors, on-the-ground technical assistance to cities, and an online platform to scale investments in urban infrastructure and services that produce local improvements on people’s lives and global sustainability benefits.</p>
+          <p className="c-text -dark -fs-medium -fw-light">Financing Sustainable Cities aims to support the growing ecosystem of people working to <strong>reduce global emissions and improve the quality of life</strong> for urban residents around the world.</p>
+          <div className="picture -buildings">
+            <p className="c-text -caption -light -fs-extrabig -fw-light">
+              Financing Sustainable Cities aims to support the growing ecosystem of people working to <strong>reduce global emissions and improve the quality of life</strong> for urban residents around the world.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="c-detail-section -content-padding">
+      <div className="row">
+        <div className="column small-4">
+          <h2 className="c-title -dark -fs-extrabig -fw-light">The initiative</h2>
+        </div>
+        <div className="column small-8">
+          <p className="c-text -dark -fs-medium -fw-light">Financing Sustainable Cities is an initiative of focused on <strong>helping cities develop business models</strong> that can accelerate the implementation of sustainable urban solutions.</p>
+          <p className="c-text -dark -fs-medium -fw-light">Cities are uniquely positioned to deliver sustainable solutions that can positively impact the lives of over half the world’s population. But, while investment capital and demand for sustainable solutions are in good supply but structuring successful sustainable projects can be a complex, time-consuming process.</p>
+          <p className="c-text -dark -fs-medium -fw-light">Solutions such as mass public transit, bike sharing systems and energy efficient buildings need a <strong>strong business model behind them to come to life.</strong> The challenge is that supply and demand are often mismatched: cities’ sustainable infrastructure projects (the “demand side”) are not seen as viable investment opportunities by the investor community (the “supply side”) as they are too small, or too risky relative to expected returns.</p>
+          <p className="c-text -dark -fs-medium -fw-light">This is particularly pronounced in emerging markets, where such investments are critical. The mismatch results in too few “bankable” projects, or investment-ready projects that are attractive enough for investors.</p>
+          <p className="c-text -dark -fs-medium -fw-light">Yet Cities worldwide are experimenting with new ways to invest in sustainable solutions, but progress often remains isolated and does not result in scalable and replicable change. Developing a common understanding of <strong>smart approaches to building cities</strong> will move us from talk to action.</p>
+          <p className="c-text -dark -fs-medium -fw-light">The current focus of the initiative is on helping cities develop business models for:</p>
+        </div>
+      </div>
+    </div>
+    <div className="c-detail-section -content-padding -content-separator">
+      <div className="row">
+        <div className="column small-4">
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Solutions</h2>
+        </div>
+        <div className="column small-4">
+          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-bike" /></svg>
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Bike sharing systems</h2>
+          <p>Cities increasingly recognize the impact bike-sharing systems can have on their broader mobility landscape. In recent years, bike-share has gone viral around the world, with 1 million shared urban bicycles in operation in 2015. The trend started in Europe, expanded to North America and has recently spread to cities in Asia and Latin America</p>
+        </div>
+        <div className="column small-4">
+          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-buildings" /></svg>
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Bus rapid transit systems</h2>
+          <p>Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id ultricies vehicula ut id elit.</p>
+        </div>
+        <div className="column small-4"></div>
+        <div className="column small-4">
+          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-bus" /></svg>
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Low- and zero-emission buses</h2>
+          <p>Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id ultricies vehicula ut id elit.</p>
+        </div>
+        <div className="column small-4">
+          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-city" /></svg>
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Efficient new buildings</h2>
+          <p>Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id ultricies vehicula ut id elit.</p>
+        </div>
+        <div className="column small-4"></div>
+        <div className="column small-4">
+          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-gov" /></svg>
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Municipal building retrofits</h2>
+          <p>Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id ultricies vehicula ut id elit.</p>
+        </div>
+        <div className="column small-4">
+          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-metro" /></svg>
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Transit-oriented development</h2>
+          <p>Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id ultricies vehicula ut id elit.</p>
+        </div>
+        <div className="column small-4"></div>
+        <div className="column small-4">
+          <svg className="icon -dark -in-line-left -big -short"><use xlinkHref="#icon-home-about" /></svg>
+          <h2 className="c-title -dark -fs-extrabig -fw-light">More coming soon</h2>
+        </div>
+        <div className="column small-4"></div>
+        <div className="column small-4"></div>
+        <div className="column small-8">
+          <div className="content"></div>
+        </div>
+      </div>
+    </div>
+    <div className="c-detail-section -content-padding">
+      <div className="row">
+        <div className="column small-4">
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Continue reading</h2>
+        </div>
+        <div className="column small-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">Events</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">Blogs</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-4"></div>
+        <div className="column small-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">City Support</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">More information</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>);
 }

612c93e05b2085ed8da67566e8446a854702f254
Clara Linos <clara.linos@gmail.com>
2017-08-07 19:09:20 +0200

    about

--- components/about/Blogs.js
+++ components/about/Blogs.js
@@ -2,6 +2,98 @@ import React from 'react';
 
 export default function Blogs() {
   return (<div className="c-about-content">
-    Blogs
+    <div className="c-detail-section -content-padding -content-separator">
+      <div className="row">
+        <div className="column small-12">
+          <h2 className="c-title -dark -fs-huge -fw-thin">FSCI Posts</h2>
+        </div>
+        <div className="column small-12 medium-4">
+          <a target="_blank" href="http://thecityfix.com/blog/whats-holding-back-latin-american-cities-clean-bus-transition-sebastian-castellanos-anne-maassen/?utm_source=rss&utm_medium=rss&utm_campaign=whats-holding-back-latin-american-cities-clean-bus-transition-sebastian-castellanos-anne-maassen" className="post">
+              <div className="picture -post-1"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">What’s Holding Back Latin American Cities’ Clean Bus Transition?</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">march 3, 2017</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-4">
+          <a target="_blank" href="http://thecityfix.com/blog/4-keys-unlock-innovative-urban-service-for-all-anne-maassen-benoit-lefevre/" className="post">
+              <div className="picture -post-2"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">4 Keys to Unlock Innovative Urban Services for All</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">february 18, 2016</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-4">
+          <a target="_blank" href="http://www.c40.org/blog_posts/4-ways-cities-are-financing-climate-action" className="post">
+              <div className="picture -post-3"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">4 ways cities are financing climate action</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">february 2, 2017</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-4">
+          <a target="_blank" href="http://thecityfix.com/blog/live-from-transforming-transportation-2017-leadership-and-private-finance-drive-a-safe-mobility-shift-alex-rogala-talia-rubnitz/?utm_source=rss&utm_medium=rss&utm_campaign=live-from-transforming-transportation-2017-leadership-and-private-finance-drive-a-safe-mobility-shift-alex-rogala-talia-rubnitz" className="post">
+              <div className="picture -post-4"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">Live from Transforming Transportation 2017: What’s Next for the Sustainable Mobility Narrative?</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">january 12, 2017</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-4">
+          <a target="_blank" href="http://thecityfix.com/blog/why-sustainable-urban-development-needs-better-finance-mark-watts-ani-dasgupta-brandee-mchale/?utm_source=rss&utm_medium=rss&utm_campaign=why-sustainable-urban-development-needs-better-finance-mark-watts-ani-dasgupta-brandee-mchale" className="post">
+              <div className="picture -post-5"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">Why Sustainable Urban Development Needs Better Finances</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">october 18, 2016</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-4">
+          <a target="_blank" href="http://www.c40.org/blog_posts/cape-town-mayor-patricia-de-lille-on-financing-and-delivering-sustainable-and-inclusive-transit-oriented-development" className="post">
+              <div className="picture -post-6"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">Mayors Voices: Cape Town Mayor Patricia de Lille on financing and delivering sustainable and inclusive transit-oriented development</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">october 10, 2016</span>
+            </a>
+        </div>
+      </div>
+
+
+      <div className="column small-12 medium-8 medium-offset-4">
+        <div className="content"></div>
+      </div>        
+    </div>
+
+    <div className="c-detail-section -content-padding">
+      <div className="row">
+        <div className="column small-12 medium-4">
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Continue reading</h2>
+        </div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">Events</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">City Support</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-12 medium-4"></div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">More information</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">About the initiative</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+      </div>
+    </div>
+
   </div>);
 }
--- components/about/Events.js
+++ components/about/Events.js
@@ -2,6 +2,97 @@ import React from 'react';
 
 export default function Events() {
   return (<div className="c-about-content">
-    Events
+    <div className="c-detail-section -content-padding -content-separator">
+      <div className="row">
+        <div className="column small-12">
+          <h2 className="c-title -dark -fs-huge -fw-thin">FSCI Events</h2>
+        </div>
+        <div className="column small-12 medium-6">
+          <a target="_blank" href="http://www.c40.org/events/c40-financing-sustainable-cities-forum-2017" className="event">
+              <div className="picture -event-1"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">2017 Financing Sustainable Cities Forum</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">April 4, 2017</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-6">
+          <a target="_blank" href="http://www.c40.org/events/c40-financing-sustainable-cities-forum-2016" className="event">
+              <div className="picture -event-2"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">Financing Sustainable Cities Forum & Workshop</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">April, 2016</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-6">
+          <a target="_blank" href="http://www.wrirosscities.org/news/workshop-mexico-city-explores-opportunities-and-barriers-net-zero-energy-buildings-mexico" className="event">
+              <div className="picture -event-3"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">Workshop in Mexico City Explores Opportunities and Barriers for Net Zero Energy Buildings in Mexico</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">March 22, 2016</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-6">
+          <a target="_blank" href="http://www.wrirosscities.org/news/workshop-india-explores-options-financing-transit-oriented-development-cities" className="event">
+              <div className="picture -event-4"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">Workshop in India Explores Options for Financing Transit-Oriented Development in Cities</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">february 25, 2016</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-6">
+          <a target="_blank" href="http://www.wrirosscities.org/news/international-seminar-discusses-financing-transit-oriented-development-tod-brazilian-cities" className="event">
+              <div className="picture -event-5"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">International Seminar Discusses Financing Transit-Oriented Development (TOD) in Brazilian Cities</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">november 11, 2015</span>
+            </a>
+        </div>
+
+        <div className="column small-12 medium-6">
+          <a target="_blank" href="http://www.wrirosscities.org/news/workshop-mexico-city-brings-together-local-leaders-building-efficiency-solutions" className="event">
+              <div className="picture -event-6"></div>
+              <p className="c-text -dark -fs-medium -fw-light -lh-small">Workshop in Mexico City brings together local leaders for building efficiency solutions</p>
+              <span className="c-text -dark -fs-smaller -fw-light -uppercase">august 19, 2015</span>
+            </a>
+        </div>
+      </div>
+
+      <div className="column small-12 medium-8 medium-offset-4">
+        <div className="content"></div>
+      </div>        
+    </div>
+
+    <div className="c-detail-section -content-padding">
+      <div className="row">
+        <div className="column small-12 medium-4">
+          <h2 className="c-title -dark -fs-extrabig -fw-light">Continue reading</h2>
+        </div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">Blogs</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">City Support</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-12 medium-4"></div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">More information</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+        <div className="column small-12 medium-4">
+          <a className="main-link -border -no-height" href="#">
+            <p className="c-text -dark -fs-medium -fw-light">About the initiative</p>
+            <p className="c-title -dark">Lorem ipsum casius tesebe aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </a>
+        </div>
+      </div>
+    </div>
+
   </div>);
 }

5a3a406d63b378386d9d01bdb49451596e45f094
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-08-23 20:46:34 +0200

    Updates icons and its sizes

--- components/about/Solutions.js
+++ components/about/Solutions.js
@@ -18,35 +18,35 @@ export default function Solutions() {
           <h2 className="c-title -dark -fs-extrabig -fw-light">Solutions</h2>
         </div>
         <div className="column small-12 medium-4">
-          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-bike" /></svg>
-          <h2 className="c-title -dark -fs-extrabig -fw-light">Bike sharing systems</h2>
+          <svg className="icon -huge -dark -in-line-left"><use xlinkHref="#icon-bike" /></svg>
+          <h2 className="c-title -huge -dark -fs-extrabig -fw-light">Bike sharing systems</h2>
           <p>Cities increasingly recognize the impact bike-sharing systems can have on their broader mobility landscape. In recent years, bike-share has gone viral around the world, with 1 million shared urban bicycles in operation in 2015. The trend started in Europe, expanded to North America and has recently spread to cities in Asia and Latin America.</p>
         </div>
         <div className="column small-12 medium-4">
-          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-buildings" /></svg>
-          <h2 className="c-title -dark -fs-extrabig -fw-light">Bus rapid transit systems</h2>
+          <svg className="icon -huge -dark -in-line-left"><use xlinkHref="#icon-buildings" /></svg>
+          <h2 className="c-title -huge -dark -fs-extrabig -fw-light">Bus rapid transit systems</h2>
           <p>Bus Rapid Transit (BRT) systems provide a cost-effective and high quality alternative to rail-based transport. Recently, many cities are recognizing the potential of BRT as a more reliable, faster, and more convenient public transport option. Successful BRT systems in Latin America have changed how we view bus-based transport, and today cities in Africa, Europe, North America and Asia are increasingly turning to these systems to address issues of informal transport and traffic congestion. </p>
         </div>
         <div className="column small-12 medium-4"></div>
         <div className="column small-12 medium-4">
-          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-bus" /></svg>
-          <h2 className="c-title -dark -fs-extrabig -fw-light">Low and zero-emission buses</h2>
+          <svg className="icon -huge -dark -in-line-left"><use xlinkHref="#icon-bus" /></svg>
+          <h2 className="c-title -huge -dark -fs-extrabig -fw-light">Low and zero-emission buses</h2>
           <p>Low and zero-emission buses can create economic, environmental and health benefits. In general, zero-emission buses have lower operating costs than traditional buses, given savings from reduced fuel consumption and maintenance. They can also help cut emissions that contribute to poor local air quality and global climate change. Furthermore, less noise pollution and vibration make for a more enjoyable journey for passengers and drivers and improve livability for residents along transit routes. </p>
         </div>
         <div className="column small-12 medium-4">
-          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-city" /></svg>
-          <h2 className="c-title -dark -fs-extrabig -fw-light">Efficient new buildings</h2>
+          <svg className="icon -huge -dark -in-line-left"><use xlinkHref="#icon-city" /></svg>
+          <h2 className="c-title -huge -dark -fs-extrabig -fw-light">Efficient new buildings</h2>
           <p>Buildings consume one-third of the world’s energy and use large amounts of other resources, including materials and water. Cities worldwide are taking action to make new buildings more resource efficient. Increases in new building stock is expected predominantly in emerging and developing cities, where most of urban growth is expected. These cities need to find creative ways to drive real estate development toward delivering efficient new buildings.</p>
         </div>
         <div className="column small-12 medium-4"></div>
         <div className="column small-12 medium-4">
-          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-gov" /></svg>
-          <h2 className="c-title -dark -fs-extrabig -fw-light">Municipal building retrofits</h2>
+          <svg className="icon -huge -dark -in-line-left"><use xlinkHref="#icon-gov" /></svg>
+          <h2 className="c-title -huge -dark -fs-extrabig -fw-light">Municipal building retrofits</h2>
           <p>Investments in efficient building retrofits promote energy efficiency and resource conservation, resulting in reduced GHG emissions and energy requirements. A Municipal Building Retrofit is a renovation of a public or municipal-owned and managed building which results in more efficient use of energy. These renovations can affect the building’s infrastructure, special equipment or interior elements, and can include the addition of green landscaping or renewable energy equipment installation.</p>
         </div>
         <div className="column small-12 medium-4">
-          <svg className="icon -dark -in-line-left -big"><use xlinkHref="#icon-metro" /></svg>
-          <h2 className="c-title -dark -fs-extrabig -fw-light">Transit-oriented development</h2>
+          <svg className="icon -huge -dark -in-line-left"><use xlinkHref="#icon-metrop" /></svg>
+          <h2 className="c-title -huge -dark -fs-extrabig -fw-light">Transit-oriented development</h2>
           <p>The world is experiencing unprecedented urbanization. Cities around the globe are exploring the potential of TOD to manage urban expansion and density. Historically, TOD has been used most in North American and East Asian cities. While relatively new to other geographies, many elements of TOD - such as mixed land use, affordable housing and public transit - are already present in the master plans of major Brazilian and South African cities, and in India, the Ministry of Urban Development approved the Transit Oriented Development Policy for Delhi in 2015. </p>
         </div>
         <div className="column small-12 medium-4"></div>

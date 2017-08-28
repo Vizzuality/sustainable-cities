b55d6f4d45e4262674b1441164cb79c788b62a89
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-08-28 16:46:58 +0200

    Restores download modal display

--- pages/explore/ExploreIndex.js
+++ pages/explore/ExploreIndex.js
@@ -275,7 +275,7 @@ class ExploreIndex extends Page {
         />
 
         <Modal
-          open={true || this.state.modal.download}
+          open={this.state.modal.download}
           toggleModal={v => this.setState({ modal: { download: v } })}
           loading={loadingBmes || loadingCities}
         >

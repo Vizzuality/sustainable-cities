1a986fbf9b23f43dc9c701997194919a0b3407ee
Clara Linos <clara.linos@gmail.com>
2017-09-12 13:33:30 +0200

    footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -7,14 +7,14 @@ export default () => (
       <div className="columns small-12">
         <div className="l-footer">
           <div className="row">
-            <div className="columns small-12 medium-4">
+            <div className="columns small-8 medium-4">
               <p className="c-text -fs-small -dark">A partnership between </p>
               <div className="logos-wrap">
                 <a href="http://www.wrirosscities.org/" target="_blank" rel="noopener noreferrer" className="logo -footer wri" />
                 <a href="http://www.c40.org/" target="_blank" rel="noopener noreferrer" className="logo -footer c40" />
               </div>
             </div>
-            <div className="columns small-12 medium-2">
+            <div className="columns small-4 medium-2">
               <p className="c-text -fs-small -dark">Funded by </p>
               <a href="http://www.citigroup.com/citi/foundation/" target="_blank" rel="noopener noreferrer" className="logo -footer citi-foundation" />
             </div>
@@ -22,16 +22,16 @@ export default () => (
             <div className="columns small-12 medium-6">
               <ul className="footer-nav">
                 <li>
-                  <Link prefetch route="about"><a className="c-text -dark -uppercase -fs-default">about</a></Link>
+                  <Link prefetch route="about"><a className="c-text -dark -uppercase -fs-smaller">about</a></Link>
                 </li>
                 <li>
-                  <Link prefetch route="explore-index"><a className="c-text -dark -uppercase -fs-default">Solutions map</a></Link>
+                  <Link prefetch route="explore-index"><a className="c-text -dark -uppercase -fs-smaller">Solutions map</a></Link>
                 </li>
                 <li>
-                  <Link prefetch route="builder"><a className="c-text -dark -uppercase -fs-default">Design</a></Link>
+                  <Link prefetch route="builder"><a className="c-text -dark -uppercase -fs-smaller">Design</a></Link>
                 </li>
                 <li>
-                  <a className="c-text -dark -uppercase -fs-default" href="mailto:amaassen@wri.org?subject=Hello from the fsci site">Contact</a>
+                  <a className="c-text -dark -uppercase -fs-smaller" href="mailto:amaassen@wri.org?subject=Hello from the fsci site">Contact</a>
                 </li>
               </ul>
             </div>

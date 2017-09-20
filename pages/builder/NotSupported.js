cebfbb58bf81fd79273f47230bb566497633d7a0
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-09-20 16:16:01 +0200

    Adds disclaimer for mobile builder

---
+++ pages/builder/NotSupported.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotSupported() {
+  return (
+    <div className="c-not-supported">
+      <p className="no-supported-text">For a full experience of the builder,<br /> please go to the desktop version.</p>
+    </div>
+  );
+}
